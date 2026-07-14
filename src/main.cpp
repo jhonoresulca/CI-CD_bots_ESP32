94d453e57d0169d3cdb52f01b657fa8996350940
Jhon Ore <jhon.ore@hotmail.com>
2026-07-13 22:36:29 -0500

    Actualiza versión 1.0.5

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #define FIRMWARE_URL "https://github.com/jhonoresulca/CI-CD_bots_ESP32/releases/latest/download/firmware.bin"
 
 // Versión actual del firmware — cambiar en cada release
-String VERSION_LOCAL = "1.0.4";
+String VERSION_LOCAL = "1.0.5";
 
 unsigned long lastCheck = 0;
 
@@ -106,7 +106,7 @@ void setup() {
     Serial.println("\n=== OTA desde GitHub ===");
 
     // Conectar a WiFi
-    WiFi.begin("Jhon_Ore2", "123456789");
+    WiFi.begin("AMERICANET_FC4D00", "jhonfernando2019");
     int attempts = 0;
     while (WiFi.status() != WL_CONNECTED && attempts < 20) {
         delay(500);
@@ -126,7 +126,7 @@ void loop() {
     // Parpadeo LED para confirmar que el firmware está corriendo
     digitalWrite(2, HIGH);
     Serial.println("LED ON");
-    delay(1000);
+    delay(100);
 
     digitalWrite(2, LOW);
     Serial.println("LED OFF");
